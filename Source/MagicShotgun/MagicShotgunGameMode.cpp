c4c6cb4b6d620bfb1b4cd607734d80adfff25302
davidkol <davidkolop@gmail.com>
2017-07-30 21:04:14 -0700

    Enabled Git LFS and added FPS content

---
+++ Source/MagicShotgun/MagicShotgunGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "MagicShotgunGameMode.h"
+#include "MagicShotgunHUD.h"
+#include "MagicShotgunCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMagicShotgunGameMode::AMagicShotgunGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AMagicShotgunHUD::StaticClass();
+}
