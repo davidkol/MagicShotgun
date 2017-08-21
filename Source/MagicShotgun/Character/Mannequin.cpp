5a747adc0a36dfd99749faf3da58dbeb8ed6caa6
davidkol <davidkolop@gmail.com>
2017-08-20 19:10:55 -0700

    1.43 both fp and tp guns recoil

--- Source/MagicShotgun/Character/Mannequin.cpp
+++ Source/MagicShotgun/Character/Mannequin.cpp
@@ -47,7 +47,8 @@ void AMannequin::BeginPlay()
 	{
 		Gun->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint_0")); 
 	}
-	Gun->AnimInstance = GetMesh()->GetAnimInstance();
+	Gun->AnimInstance1P = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance3P = GetMesh()->GetAnimInstance();
 
 	if (InputComponent != nullptr) 
 	{
--- Source/MagicShotgun/Player/FirstPersonCharacter.cpp
+++ Source/MagicShotgun/Player/FirstPersonCharacter.cpp
@@ -81,7 +81,7 @@ void AFirstPersonCharacter::BeginPlay()
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
-	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	Gun->AnimInstance1P = Mesh1P->GetAnimInstance();
 	InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
