3192fda0b057eee935cde27c695817bf03f3d4d2
davidkol <davidkolop@gmail.com>
2017-08-23 00:06:41 -0700

    1.51 grass spawn flyweights

--- Source/MagicShotgun/Terrain/Tile.h
+++ Source/MagicShotgun/Terrain/Tile.h
@@ -15,9 +15,9 @@ public:
 	// Sets default values for this actor's properties
 	ATile();
 
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	UFUNCTION(BlueprintCallable, Category = "Environment")
 	void PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 500.f, float MinScale = 0.5f, float MaxScale = 3.0f);
-
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
