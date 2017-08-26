682a0d9029b197329576e73a7595e98771ab70b6
davidkol <davidkolop@gmail.com>
2017-08-26 12:06:32 -0700

    1.65 SETUP assets, templates, blueprints for starting real game

--- Source/MagicShotgun/Terrain/Tile.cpp
+++ Source/MagicShotgun/Terrain/Tile.cpp
@@ -34,7 +34,10 @@ void ATile::BeginPlay()
 
 void ATile::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-	Pool->Return(NavMeshBoundsVolume);
+	if (Pool != nullptr && NavMeshBoundsVolume != nullptr)
+	{
+		Pool->Return(NavMeshBoundsVolume);
+	}
 }
 
 // Called every frame
